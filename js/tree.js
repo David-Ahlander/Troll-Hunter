9578ab2c03d48f935367b5fe67cf83dcd0e197c2
Albert Arvidsson <albert.arvidsson@gmail.com>
2014-03-05 19:06:34 +0100

    Fix Tree constructor
    Returnera this i randomizePostion så att man kan fortsätta använda
    trädobjektet.
    ex: new Tree().randomizePosition().sprite.render(ctx)

--- js/tree.js
+++ js/tree.js
@@ -1,12 +1,13 @@
 (function() {
-    function Tree(pos) {
-        var x = pos[0],
-            y = pos[1];
-        this.sprite = new Sprite('img/tree.png', [x, y], [120, 108]);
+    function Tree(opts) {
+        opts = opts || {};
+        this.pos = opts.pos || [0, 0];
+        this.sprite = new Sprite('img/tree.png', [0, 0], [120, 108]);
     }
 
     Tree.prototype.randomizePosition = function () {
         this.pos = [Math.random() * 680, Math.random() * 492];
+        return this;
     };
 
     window.Tree = Tree;
